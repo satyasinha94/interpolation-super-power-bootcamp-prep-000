685ddc19fd20c7b941751f13f2d52008db4a20fa
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-09-27 16:25:32 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,9 @@
 def display_rainbow(colors)
-  print "R: #{colors[0], }"
-  print "O: #{colors[1], }"
-  print "Y: #{colors[2], }"
-  print "G: #{colors[3], }"
-  print "B: #{colors[4], }"
-  print "I: #{colors[5], }"
+  print "R: #{colors[0]}, "
+  print "O: #{colors[1]}, "
+  print "Y: #{colors[2]}, "
+  print "G: #{colors[3]}, "
+  print "B: #{colors[4]}, "
+  print "I: #{colors[5]}, "
   print "V: #{colors[6]}"
 end
